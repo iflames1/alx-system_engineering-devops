f272fb09f4e7fb34e0deabcd9aa9454cb243cb05
Flames <isaacomenuche@gmail.com>
2024-06-28 20:05:22 +0100

    install this guy werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # install flask version 2.1.0 from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
 }
