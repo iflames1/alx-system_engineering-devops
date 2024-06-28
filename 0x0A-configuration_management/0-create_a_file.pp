f376b028dab31f33cdeea788bc427f08138e3fd3
Flames <isaacomenuche@gmail.com>
2024-06-28 18:53:40 +0100

    re-align

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 file { '/tmp/school':
-  ensure  => 'file',
   path    => '/tmp/school',
-  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
